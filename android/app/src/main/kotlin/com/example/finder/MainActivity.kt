6a6bba8a09ca1e37870ccc0523a370bad701c7aa
Shervin <68682979+shervinbdndev@users.noreply.github.com>
2022-12-15 12:33:08 +0330

    Commited

--- android/app/src/main/kotlin/com/example/finder/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.finder
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/constants/platform_urls.dart
+++
@@ -1,5 +0,0 @@
-const String github = 'https://github.com';
-const String reddit = 'https://reddit.com';
-const String pypi = 'https://pypi.org';
-const String linkedin = 'https://linkedin.com';
-const String instagram = 'https://instagram.com';
